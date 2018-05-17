ec8105cc096f9baf8d633c645edfee35f421edb7
temitope.oteyowo <temitope.oteyowo@tajawal.com>
2018-05-17 16:29:59 +0400

    update

--- test/idempotence.sh
+++ test/idempotence.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
-echo "#####################################\nCheck idempotence nature of the role:\n#####################################"
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
+
+echo "${YELLOW}#####################################${NC}\n${GREEN}Check idempotence nature of the role:${NC}\n${YELLOW}#####################################${NC}"
 ansible-playbook -i test/inventory test/ci-test.yml --connection=local | grep -q 'changed=0.*failed=0' && (echo 'Idempotence test: pass' && exit 0) || (echo 'Idempotence test: fail' && exit 1)
--- test/yamllint.sh
+++ test/yamllint.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
 changedfiles=$(git show --pretty="" --no-commit-id  --diff-filter=AMRC  --name-only `git log --format="%H" -n 1` | grep -e .yml$  | tr -s "\n" " " | sed 's/%$//')
 echo $changedfiles
-if [ ! -z $changedfiles ]; then echo $changedfiles | xargs yamllint -c ./test/yamllint; else echo "No yaml file changes"; fi
+if [ ! -z $changedfiles ]; then echo $changedfiles | xargs yamllint -c ./test/yamllint; else echo "${GREEN}No yaml file changes${NC}"; fi
