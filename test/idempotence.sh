c6f4e398fdc1e5fe63afdfda647f20633925e5c6
temitope.oteyowo <temitope.oteyowo@tajawal.com>
2018-05-17 16:34:54 +0400

    update

--- test/idempotence.sh
+++ test/idempotence.sh
@@ -5,5 +5,5 @@ YELLOW='\033[1;33m'
 NC='\033[0m'
 
 
-echo "${YELLOW}#####################################${NC}\n${GREEN}Check idempotence nature of the role:${NC}\n${YELLOW}#####################################${NC}"
+echo "${YELLOW}#####################################${NC}\n${YELLOW}#${NC} ${GREEN}Check idempotence nature of the role#${NC}\n${YELLOW}#####################################${NC}"
 ansible-playbook -i test/inventory test/ci-test.yml --connection=local | grep -q 'changed=0.*failed=0' && (echo 'Idempotence test: pass' && exit 0) || (echo 'Idempotence test: fail' && exit 1)
