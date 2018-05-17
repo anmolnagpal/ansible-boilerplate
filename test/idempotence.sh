bb951940fa2f94886409030fe064343c2fe5d50d
temitope.oteyowo <temitope.oteyowo@tajawal.com>
2018-05-17 10:48:07 +0400

    update

--- test/idempotence.sh
+++ test/idempotence.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 echo "#####################################\nCheck idempotence nature of the role:\n#####################################"
-cd ../
-"ansible-playbook -i test/inventory test/ci-test.yml --connection=local | grep -q 'changed=0.*failed=0' && (echo 'Idempotence test: pass' && exit 0) || (echo 'Idempotence test: fail' && exit 1)"
+ansible-playbook -i test/inventory test/ci-test.yml --connection=local | grep -q 'changed=0.*failed=0' && (echo 'Idempotence test: pass' && exit 0) || (echo 'Idempotence test: fail' && exit 1)
